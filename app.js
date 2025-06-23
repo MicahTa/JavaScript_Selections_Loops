b6ed05b793b821fa6b1340dc3fb10ad8f20d7df2
Laptop <micahcorbintanner@gmail.com>
2025-06-23 06:11:50 -0600

    Did the doo

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-console.log("Hello World!\n==========\n");
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log(`Fizz ${fizzDivisor}`);
+console.log(`Buzz ${buzzDivisor}`);
+console.log(`Start ${start}`);
+console.log(`End ${end}`);
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+for (let i = start; i<=end; i++) {
+    console.log(i);
+    if (i%fizzDivisor == 0) {
+        console.log("fizz");
+    }
+    if (i%buzzDivisor == 0) {
+        console.log("buzz");
+    }
+}
